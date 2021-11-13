6107bc664eb241a3b9911e79b535a519de9d6f9a
Brian Clincy <8976115+bclincy@users.noreply.github.com>
2021-11-13 16:20:24 -0500

    install script

--- brew.sh
+++ brew.sh
@@ -1,46 +1,72 @@
-tap "hashicorp/tap"
-tap "heroku/brew"
-tap "homebrew/bundle"
-tap "homebrew/cask"
-tap "homebrew/core"
-tap "homebrew/services"
-tap "shivammathur/php"
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+echo "Installing cask..."
+CASKS=(
+    iterm2
+    google-backup-and-sync
+    firefox
+    font-source-code-pro
+    slack
+    intellij-idea-ce
+    visual-studio-code
+    steam
+    google-chrome
+    alfred 
+    1password
+    phpstorm
+    macdown
+)
+echo "Installing cask apps..."
+brew cask install ${CASKS[@]}
+
+brew tap homebrew/cask-fonts
+brew tap "hashicorp/tap"
+brew tap "heroku/brew"
+brew tap "homebrew/bundle"
+brew tap "homebrew/cask"
+brew tap "homebrew/core"
+brew tap "homebrew/services"
+brew tap "shivammathur/php"
 # Informative, fancy bash prompt for Git users
-brew "bash-git-prompt"
+brew install "bash-git-prompt"
 # Open source suite of directory software
-brew "openldap"
-# Get a file from an HTTP, HTTPS or FTP server
-brew "curl"
+brew install "openldap"
 # Interpreted, interactive, object-oriented programming language
-brew "python@3.9"
+brew install "python"
 # Core application library for C
-brew "glib"
+#brew "glib"
 # Toolkit for embedding hypervisor capabilities in your application
-brew "hyperkit", link: false
+brew install "hyperkit", link: false
 # Postgres C API library
-brew "libpq"
+brew install "libpq"
 # Run a Kubernetes cluster locally
-brew "minikube"
+brew install "minikube"
 # Open source relational database management system
-brew "mysql-client"
+brew install "mysql-client"
 # HTTP/2 C Library
-brew "nghttp2"
+#brew "nghttp2"
 # Libraries for security-enabled client and server applications
-brew "nss"
+brew install "nss"
 # General-purpose scripting language
-brew "php@7.4", restart_service: true
+brew install "php@7.4", restart_service: false
+brew install "php@8.1", restart_service: false
 # Tool Command Language
-brew "tcl-tk"
+brew install "tcl-tk"
 # Terraform
-brew "hashicorp/tap/terraform", link: false
+brew install "hashicorp/tap/terraform", link: false
 # Everything you need to get started with Heroku
-brew "heroku/brew/heroku"
+brew install "heroku/brew/heroku"
 # General-purpose scripting language
-brew "shivammathur/php/php"
-brew git
-brew bash
+brew install "shivammathur/php/php"
+#brew install git
+#brew install bash
 # Interactive JavaScript, Python or SQL interface
-cask "mysql-shell"
-cask "alfred"
-cask "google-chrome"
-cask "visual-studio-code"
+brew install node
+
+npm -v
